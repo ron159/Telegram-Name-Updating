980a674467091c44454f8c61cc197cf8af6ca505
ron159 <zlronron159@gmail.com>
2019-05-15 15:51:47 +0800

    修改了一些设置

--- tg_username_update.py
+++ tg_username_update.py
@@ -4,6 +4,7 @@
 # Updated:
 #  1. 使用async来update lastname，更加稳定
 #  2. 增加emoji clock，让时间显示更加有趣味
+# fork from cody
 
 import time
 import os
@@ -17,8 +18,8 @@ from telethon.tl.functions.account import UpdateProfileRequest
 from emoji import emojize
 
 
-dizzy = emojize(":dizzy:", use_aliases=True)
-cake = emojize(":cake:", use_aliases=True)
+no_smoking = emojize(":no_smoking:", use_aliases=True)
+dash = emojize(":dash:", use_aliases=True)
 all_time_emoji_name = ["clock12", "clock1230", "clock1", "clock130", "clock2", "clock230", "clock3", "clock330", "clock4", "clock430", "clock5", "clock530", "clock6", "clock630", "clock7", "clock730", "clock8", "clock830", "clock9", "clock930", "clock10", "clock1030", "clock11", "clock1130"]
 time_emoji_symb = [emojize(":%s:" %s, use_aliases=True) for s in all_time_emoji_name]
 
@@ -41,20 +42,19 @@ async def change_name_auto():
     # ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
     # https://stackoverflow.com/questions/4788533/python-strftime-gmtime-not-respecting-timezone
 
-    print('will change name')
+    print('Going to change name')
 
     while True:
         try:
             time_cur = strftime("%H:%M:%S:%p:%a", time.localtime())
             hour, minu, seco, p, abbwn = time_cur.split(':')
-            if seco=='00' or seco=='30':
+            if seco=='00' or seco=='20' or seco=='40':
                 shift = 0
                 mult = 1
                 if int(minu)>30: shift=1
-                # print((int(hour)%12)*2+shift)
                 # hour symbols
                 hsym = time_emoji_symb[(int(hour)%12)*2+shift]
-                # await client1.send_message('me', hsym)
+                
                 for_fun = random.random() 
                 if for_fun < 0.10:
                     last_name = '%s时%s分 %s' % (hour, minu, hsym)
@@ -62,10 +62,10 @@ async def change_name_auto():
                     last_name = '%s:%s %s %s %s' % (hour, minu, p, abbwn, hsym)
                 elif for_fun < 0.60:
                     last_name = '%s:%s %s UTC+8 %s' % (hour, minu, p, hsym)
-                elif for_fun < 0.90:
-                    last_name = '%s' % dizzy
+                elif for_fun < 0.80:
+                    last_name = '%s' % no_smoking
                 else:
-                    last_name = '%s' % cake
+                    last_name = '%s' % dash
         
                 await client1(UpdateProfileRequest(last_name=last_name))
                 logger.info('Updated -> %s' % last_name)
