42204460f911ea51664ef8a83fdf5d8bf5c154f6
ron159 <zlronron159@gmail.com>
2019-12-31 15:05:02 +0800

    日期bug

--- dayscal.py
+++ dayscal.py
@@ -11,7 +11,7 @@ def today_of_year():  # 计算今天是今年的多少天
     deltaDetails = utc_now - \
         datetime.datetime(utc_now.year, 1, 1, 0, 0, 0, 0) + \
         timedelta(days=1, hours=8)
-    deltaDays = deltaDetails.days-1
+    deltaDays = deltaDetails.days
 
     days = is_leap_year()
     percent = ((deltaDetails.days-1)*86400+deltaDetails.seconds) / \
