14b1c73ad6c21911f9a9bf40859271dca87d4cbf
ron159 <zlronron159@gmail.com>
2020-01-02 10:14:24 +0800

    still time fix

--- dayscal.py
+++ dayscal.py
@@ -10,18 +10,18 @@ def today_of_year():  # 计算今天是今年的多少天
     today = datetime.date(today_cal.year, today_cal.month, today_cal.day)
     deltaDetails = utc_now - \
         datetime.datetime(utc_now.year, 1, 1, 0, 0, 0, 0) + \
-        timedelta(days=1, hours=8)
-    deltaDays = deltaDetails.days
+        timedelta(hours=8)
+    deltaDays = deltaDetails.days+1
 
     days = is_leap_year()
-    percent = ((deltaDetails.days-1)*86400+deltaDetails.seconds) / \
+    percent = (deltaDetails.days*86400+deltaDetails.seconds) / \
         (days*86400)  # 计算精确到秒的百分比
 
     return today, deltaDays, percent
 
 
 def is_leap_year():  # 判断是否是闰年
-    year = datetime.datetime.now().year
+    year = (datetime.datetime.utcnow()+timedelta(hours=8)).year
     if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
         days = 366
         return days
